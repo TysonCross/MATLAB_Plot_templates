37a0b298b77e138bdfea7b90eab4f9d5609ec1e3
Tyson <tyson.cross@gmail.com>
2018-07-31 15:10:05 +0200

    Added FillAxes script

---
+++ FillAxesPos.m
@@ -0,0 +1,17 @@
+function [pos] = FillAxesPos(ax,mult)
+
+if nargin <2
+    mult = 1;
+end
+
+mult = mult*0.99;
+outerpos = ax.OuterPosition;
+ti = ax.TightInset; 
+left = outerpos(1) + ti(1);
+bottom = outerpos(2) + ti(2);
+ax_width = outerpos(3) - ti(1) - ti(3);
+ax_height = outerpos(4) - ti(2) - ti(4);
+pos = [left+((1-mult)/2) bottom+((1-mult)/2) ax_width*mult ax_height*mult];
+
+end
+
